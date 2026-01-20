e429bdb7bfe8e8ac39330eea10f7f6e64d6bdecb
Nguyen Duy <ichduy188@gmail.com>
2026-01-20 09:38:00 +0700

    refactor: simplify room opening logic in FighterRoom, enhance ViewerRooms with search filtering and pagination, and introduce useGetUserBet hook for user bet retrieval

---
+++ src/hooks/query/useGetUserBet.ts
@@ -0,0 +1,22 @@
+import { useCurrentAccount, useSuiClientContext } from "@mysten/dapp-kit";
+import { useQuery } from "@tanstack/react-query";
+
+const useGetUserBet = () => {
+  const { client } = useSuiClientContext();
+  const currentAccount = useCurrentAccount();
+    // const matchIdByFighter = useAppSelector((state) => {
+    //   return state.game.fighterRoom.match_id;
+    // });
+  
+  const query = useQuery({
+    queryKey: ['user-bet'],
+    queryFn: async () => {
+      const bet = await getUserBet();
+      return bet;
+    },
+  });
+  
+  return query;
+}
+
+export default useGetUserBet
