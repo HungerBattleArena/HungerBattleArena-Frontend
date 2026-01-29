fd5feb980d51bd5638183b26557aefc6c4dade4b
Nguyen Duy <ichduy188@gmail.com>
2026-01-29 14:42:54 +0700

    chore: update contract constants for PackageID, Registry, and Treasury addresses

--- src/constants/contract.ts
+++ src/constants/contract.ts
@@ -1,6 +1,6 @@
-export const PackageID = '0xbc13ff229ee30ea6c95b3f8880e595238dfa0e1cbdb9e6db5db63551ca5d6d7d';
-export const Registry = '0x1fa76c2a8af7d88aa2f4028071244439b75fdaa6afb9543df86be16d43947dd5';
-export const Treasury = '0x452bb2187385aba910ffb3cb00b3d5bb38da40a41ac891ba56712d8e57652678';
+export const PackageID = '0xc87396954d4926813a9118dd5524355331537004ef0b94db2c54d12605f4937c';
+export const Registry = '0x2fb20fc21dfac243a59e6958322c82ba17bc7e45e906ca8c194a193bdfcd6aa8';
+export const Treasury = '0xfb8a80d595ff1970d27d77188f414791e74cacc293438ee8fb44b0a2b269e8d0';
 
 export const coinType = '0x8b76fc2a2317d45118770cefed7e57171a08c477ed16283616b15f099391f120::hackathon::HACKATHON';
 
